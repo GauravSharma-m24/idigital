1f1132088a794d55c3eac686725336ead416f69c
Gaurav Sharma <gauravsharmam24@gmail.com>
2025-06-14 11:06:26 +0530

    Updated main_layout.blade.php

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -202,56 +202,44 @@
 
             <!-- Mobile Menu Start -->
             <div class="mobile-menu-items">
-                <ul class="nav-menu">
-                    <li><a href="index.html">Home</a></li>
-                    <li>
-                        <a href="#">All Course</a>
-                        <ul class="sub-menu">
-                            <li><a href="courses.html">Courses</a></li>
-                            <li><a href="courses-details.html">Courses Details</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">Pages </a>
-                        <ul class="sub-menu">
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="register.html">Register</a></li>
-                            <li><a href="login.html">Login</a></li>
-                            <li><a href="faq.html">FAQ</a></li>
-                            <li><a href="404-error.html">404 Error</a></li>
-                            <li><a href="after-enroll.html">After Enroll</a></li>
-                            <li><a href="courses-admin.html">Instructor Dashboard (Course List)</a></li>
-                            <li><a href="overview.html">Instructor Dashboard (Performance)</a></li>
-                            <li><a href="students.html">Students</a></li>
-                            <li><a href="reviews.html">Reviews</a></li>
-                            <li><a href="engagement.html">Course engagement</a></li>
-                            <li><a href="traffic-conversion.html">Traffic & conversion</a></li>
-                            <li><a href="messages.html">Messages</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">Blog</a>
-                        <ul class="sub-menu">
-                            <li>
-                                <a href="#">Blog</a>
-                                <ul class="sub-menu">
-                                    <li><a href="blog-grid.html">Blog</a></li>
-                                    <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
-                                    <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#">Blog Details</a>
-                                <ul class="sub-menu">
-                                    <li><a href="blog-details-left-sidebar.html">Blog Details Left Sidebar</a></li>
-                                    <li><a href="blog-details-right-sidebar.html">Blog Details Right Sidebar</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li><a href="contact.html">Contact</a></li>
-                </ul>
-
+               <ul class="nav-menu">
+                                <li><a href="{{ route('homepage') }}">Home</a></li>
+                                <li>
+                                    <a href="{{route('coursepage')}}">Our Courses</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('aboutpage') }}">About Us</a>
+                                </li>
+                                <li>
+                                    <a href="#">Packages</a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="{{ route('pro_package') }}">Pro</a>
+                                        </li>
+                                        <li>
+                                            <a href="{{ route('supreme_package') }}">Supreme</a>
+                                        </li>
+                                        <li>
+                                            <a href="{{ route('premium_package') }}">Premium</a>
+                                        </li>
+                                        <li>
+                                            <a href="{{ route('plus_package') }}">Premium Plus</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">More</a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="{{ url('/#blogs') }}">Our Blog</a>
+                                        </li>
+                                        <li>
+                                            <a href="{{ url('/#faq') }}">FAQs</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="{{ route('contactpage') }}">Contact</a></li>
+                            </ul>
             </div>
             <!-- Mobile Menu End -->
 
